240353f2cf5a0dfbff9c83605589e13f3ee60b2d
ericwm76 <eric.meldrum@gmail.com>
2020-04-04 08:30:01 -0600

    Install ant-design library; create Nav component

---
+++ src/containers/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+const Nav = () => {
+
+  return (
+    <div className="nav">
+      <h1>Juntos2College</h1>
+    </div>
+  )
+}
+
+export default Nav;

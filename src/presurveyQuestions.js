034ef8612a3e7a369fac44f694da1a99148ee1cc
ericwm76 <eric.meldrum@gmail.com>
2020-04-08 20:56:24 -0600

    Modify presurvey data

--- src/presurveyQuestions.js
+++ src/presurveyQuestions.js
@@ -1,14 +1,9 @@
 export const presurveyQuestions = [
   {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'}
+  {Eng: 'Have to help my family first', Esp: 'Primero tengo que ayudar a mi familia'},
+  {Eng: 'I\'ll never earn enough to make it worth the effort', Esp: 'Nunca ganaré lo suficiente para que valga la pena'},
+  {Eng: 'I fail too much', Esp: 'Fallo demasiado'},
+  {Eng: 'The bus takes too long', Esp: 'El bus tarda demasiado'},
+  {Eng: 'I have to live at home', Esp: 'Tengo que vivir con familia'},
+  {Eng: 'No one will make me do my homework', Esp: 'Nadie me hará hacer mi tarea'},
 ]

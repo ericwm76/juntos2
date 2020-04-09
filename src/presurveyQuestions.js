dff96f00b2bd318b367f4ed299bc6abe4b6e90fc
ericwm76 <eric.meldrum@gmail.com>
2020-04-09 08:46:35 -0600

    Pass state and props down to card component

--- src/presurveyQuestions.js
+++ src/presurveyQuestions.js
@@ -1,9 +1,11 @@
-export const presurveyQuestions = [
-  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
-  {Eng: 'Have to help my family first', Esp: 'Primero tengo que ayudar a mi familia'},
-  {Eng: 'I\'ll never earn enough to make it worth the effort', Esp: 'Nunca ganaré lo suficiente para que valga la pena'},
-  {Eng: 'I fail too much', Esp: 'Fallo demasiado'},
-  {Eng: 'The bus takes too long', Esp: 'El bus tarda demasiado'},
-  {Eng: 'I have to live at home', Esp: 'Tengo que vivir con familia'},
-  {Eng: 'No one will make me do my homework', Esp: 'Nadie me hará hacer mi tarea'},
+const presurveyQuestions = [
+  {eng: 'I don\'t want to go into debt', esp: 'No quiero endeudarme'},
+  {eng: 'Have to help my family first', esp: 'Primero tengo que ayudar a mi familia'},
+  {eng: 'I\'ll never earn enough to make it worth the effort', esp: 'Nunca ganaré lo suficiente para que valga la pena'},
+  {eng: 'I fail too much', esp: 'Fallo demasiado'},
+  {eng: 'The bus takes too long', esp: 'El bus tarda demasiado'},
+  {eng: 'I have to live at home', esp: 'Tengo que vivir con familia'},
+  {eng: 'No one will make me do my homework', esp: 'Nadie me hará hacer mi tarea'},
 ]
+
+export default presurveyQuestions;

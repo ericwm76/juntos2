0d4ce0d7d161803a8d997818eac37de7c0706cb6
ericwm76 <eric.meldrum@gmail.com>
2020-04-08 20:40:39 -0600

    Create presurvey question data

---
+++ src/presurveyQuestions.js
@@ -0,0 +1,14 @@
+export const presurveyQuestions = [
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'},
+  {Eng: 'I don\'t want to go into debt', Esp: 'No quiero endeudarme'}
+]

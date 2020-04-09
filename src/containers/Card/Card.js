eae54602bf9c16a54270876a54b2deb11749fbe1
ericwm76 <eric.meldrum@gmail.com>
2020-04-09 08:54:45 -0600

    Create click handlers for agree/disagree buttons

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -5,16 +5,24 @@ import presurveyQuestions from '../../presurveyQuestions'
 import './Card.less';
  
 const QuestionCard = ({props}) => {
-  
+  const {currentQuestion, agree, disagree} = props;
+
+  const handleAgree = () => {
+    agree();
+  }
+
+  const handleDisagree = () => {
+    disagree();
+  }
 
   return (
     <div className="card">
       <div>
         <Card style={{ width: 400, height: 300 }}>
-          <p className="question">{presurveyQuestions[props.currentQuestion].eng}</p>
+          <p className="question">{presurveyQuestions[currentQuestion].eng}</p>
           <div className="card-buttons">
-            <Button className="agree-button" type="primary" icon={<CheckOutlined />} size={"large"} shape={"circle"} />
-            <Button className="disagree-button" type="primary" icon={<CloseOutlined />} size={"large"} shape={"circle"}/>
+            <Button className="agree-button" type="primary" icon={<CheckOutlined />} size={"large"} shape={"circle"} onClick={handleAgree}/>
+            <Button className="disagree-button" type="primary" icon={<CloseOutlined />} size={"large"} shape={"circle"} onClick={handleDisagree}/>
           </div>
         </Card>
       </div>

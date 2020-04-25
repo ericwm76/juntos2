04fdc8cefe46395594e13de3e03b00ccb0a85518
ericwm76 <eric.meldrum@gmail.com>
2020-04-25 12:18:30 -0600

    Remove unnecessary imports and components from App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,24 +2,15 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Route } from 'react-router-dom';
 import Nav from '../Nav/Nav'
-
-
-import {
-  agreeWithQuestion,
-  disagreeWithQuestion
-} from '../../features/presurvey/presurveySlice'
 import { Presurvey } from '../../features/presurvey/Presurvey';
 
 const App = () => {
-  const dispatch = useDispatch()
-  const cards = useSelector(state => state.presurvey.cards)
-  const current = useSelector(state => state.presurvey.current)
 
   return (
     <>
       <Nav />
       <Route exact path="/presurvey">
-        <Presurvey id={current}/>
+        <Presurvey/>
       </Route>
     </>
   )
